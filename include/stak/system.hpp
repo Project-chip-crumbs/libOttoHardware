d78f46561e51c59afbc4408c8be48e1202ef159c
Wynter Woods <zerotri@gmail.com>
2015-03-13 14:36:47 -0700

    Adding Shutdown call stub

---
+++ include/stak/system.hpp
@@ -0,0 +1,8 @@
+#ifndef STAK_SYSTEM_HPP
+#define STAK_SYSTEM_HPP
+#include <stak/stak.hpp>
+#include <stak/types.hpp>
+
+STAK_EXPORT void stakSystemShutdown( );
+
+#endif//STAK_SYSTEM_HPP
---
+++ src/system.cpp
@@ -0,0 +1,6 @@
+#include <stak/stak.hpp>
+#include <stak/types.hpp>
+
+STAK_EXPORT void stakSystemShutdown( ) {
+  return;
+}

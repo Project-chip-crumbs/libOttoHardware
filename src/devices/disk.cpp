28d840e8db28a571b72e76b6f8d2f911b7f96e36
Wynter Woods <zerotri@gmail.com>
2015-04-15 00:29:00 +0000

    Updates to CMakeLists.txt and disk.cpp

--- src/devices/disk.cpp
+++ src/devices/disk.cpp
@@ -6,7 +6,7 @@ static void read_disk( uint64_t *total, uint64_t *used ) {
 
   // call df with a blocksize of 512 bytes and return just the last line of the
   // output so we can easily scan it for disk usage statistics
-  FILE *fp = popen( "df -B 512 / | tail -n 1", "r" );
+  FILE *fp = popen( "df -k / | tail -n 1", "r" );
   fgets( buffer, 2048, fp );
   pclose( fp );
   buffer[ 2047 ] = '\0';
@@ -18,12 +18,12 @@ static void read_disk( uint64_t *total, uint64_t *used ) {
 STAK_EXPORT uint64_t ottoDiskUsage() {
   uint64_t total = 0, used = 0;
   read_disk( &total, &used );
-  return used * 512ULL;
+  return used * 1024ULL;
 }
 
 // Return total size of user data partition in bytes
 STAK_EXPORT uint64_t ottoDiskSize() {
   uint64_t total = 0, used = 0;
   read_disk( &total, &used );
-  return total * 512ULL;
+  return total * 1024ULL;
 }
